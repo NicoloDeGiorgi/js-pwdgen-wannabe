2713d3734bf23319b18ab7517721c1f3317ff511
Nicolò <nico.2699@icloud.com>
2024-05-06 21:00:34 +0200

    add: prompt

---
+++ js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var nome = prompt("Inserisci il tuo nome"); //Variabile nome
+var cognome = prompt("Inserisci il tuo cognome"); //Variabile cognome
+var colore = prompt("Inserisci il tuo colore preferito"); // Variabile colore preferito
+
+document.body.innerHTML = nome + cognome + colore + "21"; //Variabile con password creata
