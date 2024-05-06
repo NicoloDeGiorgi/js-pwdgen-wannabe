7809f51a50aba975283921675338440833c5fbb6
Nicolò <nico.2699@icloud.com>
2024-05-06 21:07:42 +0200

    add: comment

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 'use strict';
 
+//nome-cognome-colore
+
 const nome = prompt("Inserisci il tuo nome"); //Variabile nome
 const cognome = prompt("Inserisci il tuo cognome"); //Variabile cognome
 const colore = prompt("Inserisci il tuo colore preferito"); // Variabile colore preferito
 
+//totale
 document.body.innerHTML = nome + cognome + colore + "21"; //Variabile con password creata
