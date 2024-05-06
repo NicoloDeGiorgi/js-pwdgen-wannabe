86b9518a9e739dcb42dcbb4066de2ae57e104619
Nicolò <nico.2699@icloud.com>
2024-05-06 21:05:51 +0200

    add: const

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-consts nome = prompt("Inserisci il tuo nome"); //Variabile nome
-consts cognome = prompt("Inserisci il tuo cognome"); //Variabile cognome
-consts colore = prompt("Inserisci il tuo colore preferito"); // Variabile colore preferito
+const nome = prompt("Inserisci il tuo nome"); //Variabile nome
+const cognome = prompt("Inserisci il tuo cognome"); //Variabile cognome
+const colore = prompt("Inserisci il tuo colore preferito"); // Variabile colore preferito
 
 document.body.innerHTML = nome + cognome + colore + "21"; //Variabile con password creata
